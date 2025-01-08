a52dc8c4824fabb8df24b94075ca8159ebe33f57
Oliver Kauppi <oliver.kauppii@gmail.com>
2025-01-08 17:14:22 +0200

    teht 1 valmis

--- teht1/main.cpp
+++ teht1/main.cpp
@@ -2,18 +2,21 @@
 
 using namespace std;
 
-int main()
+int game(int maxnum)
 {
     srand(time(NULL));
     int randomNum = 0;
     int userNum = 0;
+    int guessedNumCount = 0;
     while (randomNum == 0) {
-        randomNum = rand() % 21;
+        randomNum = rand() % maxnum+1;
     }
 
     while (userNum != randomNum) {
         cout << "Guess a randomly generated number: ";
         cin >> userNum;
+        ++guessedNumCount;
+        // cout << guessedNumCount;
 
         if (userNum > randomNum) {
             cout << "Guessed number is higher than random number!" << endl;
@@ -23,7 +26,21 @@ int main()
             break;
         }
     }
-    cout << "You guessed right!!" << endl;
+
+    return guessedNumCount;
+}
+
+
+int main()
+{
+    int maxnum = 0;
+    cout << "Give a maxinum number for randomizer: ";
+    cin >> maxnum;
+
+    int guesses = game(maxnum);
+
+    cout << "You guessed the correct number. Tries used: " << guesses << endl;
 
     return 0;
 }
+
