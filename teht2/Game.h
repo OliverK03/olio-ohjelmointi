9c6874bb3984d3777b9a55649a2b486e09eebb11
Oliver Kauppi <oliver.kauppii@gmail.com>
2025-01-13 15:42:53 +0200

    teht2 kesken

---
+++ teht2/Game.h
@@ -0,0 +1,23 @@
+#ifndef GAME_H
+#define GAME_H
+#include <iostream>
+
+using namespace std;
+
+class GAME_H
+{
+public:
+    Game(int);
+        ~Game();
+
+    void play();
+private:
+    int maxNumber;
+    int playerGuess;
+    int randomNumber;
+    int numOfGuesses;
+
+    void printGameResult();
+};
+
+#endif // GAME_H
